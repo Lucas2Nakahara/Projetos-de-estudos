62a786a89ac402be317be9fb414ba717e3627da1
Lucas Nakahara <nakaharalucas2@gmail.com>
2025-04-05 15:41:29 -0300

    CommitUpdate

---
+++ C#/ind.cs
@@ -0,0 +1,11 @@
+using System.Colections;
+using System.Colections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    int speed = 5;//tipo int passando o valor inteiro
+    float attackRange = 2.0f; //tipo float passando o valor com ponto flutuante
+
+    string enemyName = "Goblin"; //tipo string passando o nome entre aspas duplas
+
+}
